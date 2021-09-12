c8b2894313a9fd30be5865ba78f268335465a5b9
developerarik <shahriartajd@gmail.com>
2021-09-13 02:37:30 +0600

    home page done!

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+
+// Code for Typing Animation
+
+var typed = new Typed('.changing-heading', {
+  strings: ['provide the best services', 'make our clients happy', 'grow your business', 'make your dreams true'],
+  typeSpeed:60,
+  backSpeed:60,
+  loop:true,
+  showCursor: true,
+});
